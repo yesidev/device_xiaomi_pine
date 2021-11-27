bd05e14b5b813bef5d0e011c4d533113302f9871
hsx02 <hsx02self@gmail.com>
2021-11-27 19:43:31 +0530

    pine: set boot animation match our device display

--- aosp_pine.mk
+++ aosp_pine.mk
@@ -18,6 +18,9 @@ $(call inherit-product, device/xiaomi/pine/device.mk)
 # Broken verify
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
+# Bootanimation res
+TARGET_BOOT_ANIMATION_RES := 720
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := pine
 PRODUCT_NAME := aosp_pine
