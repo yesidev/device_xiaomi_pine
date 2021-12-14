5f3f1a95c6d19634466ce4caaef36527f2dae9a0
hsx02 <hsx02self@gmail.com>
2021-12-15 03:16:10 +0530

    Revert "build fp: Switch to raven december fp"
    This reverts commit 63fc4fa041bb774252bdf899438e0cc5339afb92.

--- spark_pine.mk
+++ spark_pine.mk
@@ -33,7 +33,7 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7957957 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SD1A.210817.036 7805805 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys"
